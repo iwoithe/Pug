300c9df84d518012b74330c9c09a5b3a6967bda2
iwoithe <iwoithe@just42.net>
2020-09-26 15:48:55 +0930

    Added the requirements, README and About.

---
+++ README.md
@@ -0,0 +1,35 @@
+# Pug
+
+Pug is a cross-platform, free and open source GUI for PIP.
+
+![Screenshot of Pug](screenshots/demo.png)
+
+## Features
+
+ - Install packages from PyPi
+ - Option to install package for current user
+ - Option to install package for either Python 2 or 3
+ - Uninstall packages
+ - Console to view progress
+
+## Todo
+
+ - [ ] Create an icon/logo
+ - [ ] Improve performance
+ - [ ] Create ``Upgrade Package``
+ - [ ] Create ``Install Requirements``
+ - [ ] Preview Package (in ``Install Package`` and ``Uninstall Package``)
+ - [ ] Create more styles
+ - [ ] Write a plugin system
+
+## Contributing
+
+Pug is a work in progress and is no where near ready for version 1.0.0. Contributions and pull requests are welcome.
+
+## Help
+
+For any bugs or feature requests, please open an issue on Gitlab.
+
+## License
+
+Pug is released under the General Public License v3 (GPLv3).
---
+++ ui/about/about.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#
+#  Copyright 2020 iwoithe <iwoithe@just42.net>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+import sys
+
+from PyQt5 import uic
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class About(QDialog):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if hasattr(kwargs, "parent"):
+            self.parent = getattr(kwargs, "parent")
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        uic.loadUi("ui/about/about.ui", self)
+        self.bind_signals()
+
+        about_text = self.get_readme()
+
+        self.about_text.setMarkdown(about_text)
+
+    def bind_signals(self):
+        self.button_box.accepted.connect(self.accept)
+
+    def get_readme(self) -> str:
+        """ Gets the text in the README file """
+        readme_text: str
+
+        with open("README.md") as f:
+            readme_text = f.read()
+
+        return readme_text
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("fusion")
+    about = About()
+    about.show()
+    sys.exit(app.exec())
